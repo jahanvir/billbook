65f02891220584d5721e50106102e7ea4a899d94
Etancher <30360093+jahanvir@users.noreply.github.com>
2023-04-09 19:56:43 +0530

    Added bill save functionality Added field checks Changed icons and app logo Added aleart dialogs

--- lib/pages/billModel.dart
+++ lib/pages/billModel.dart
@@ -4,10 +4,10 @@ import 'itemModel.dart';
 class billModel {
   String custName;
   String date;
-  List<itemModel> items;
+  List<itemModel>? items;
   billModel({
-    required this.custName,
-    required this.date,
-    required this.items,
-  });
+    this.custName='',
+    this.date='',
+
+});
 }
